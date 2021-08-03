77d9304d3378b787cde13df38b85be64643a3baa
Haubm.1995@gmail.com <Vuhuyxuan08>
2021-08-03 21:31:41 +0700

    initial commit

---
+++ src/com/javabasic/stringlesson/StringLesson.java
@@ -0,0 +1,11 @@
+package com.javabasic.stringlesson;
+
+
+import java.util.Scanner;
+
+public class StringLesson {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
